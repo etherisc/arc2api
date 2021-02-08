bc9f99c18ff4b96003dc30022f1c55d815b2b6ab
Matthias Zimmermann <zimmermann@xbrain.ch>
2021-02-08 21:47:02 +0100

    Update arc2api.R
    add functionality to extract percipitation for a region in addition to providing a single value

--- app/arc2api.R
+++ app/arc2api.R
@@ -5,15 +5,24 @@
 
 library(rnoaa)
 
-#* Return the precipitation at a date for lat, lng location
+#* Return the precipitation at a date for lat, lng bounding box location. When maximum parameters are skipped only the value for the mininum latitude/longitude is returned.
 #* @param date date
-#* @param lat latitude
-#* @param lng longitude
+#* @param latMin minimum latitude of bounding box
+#* @param lngMin minimum longitude of bounding box
+#* @param latMax maximum latitude of bounding box (optional parameter)
+#* @param lngMax maximum longitude of bounding box (optional parameter)
 #* @get /arc2
-function(date="", lat="", lng=""){
+function(date="", latMin="", lngMin="", latMax="", lngMax="") {
+   latMinFloat <- round(as.numeric(latMin), digits = 1)
+   lngMinFloat <- round(as.numeric(lngMin), digits = 1)
 
-  latFloat <- round(as.numeric(lat), digits = 1)
-  lngFloat <- round(as.numeric(lng), digits = 1)
-  box <- c(xmin = lngFloat, ymin = latFloat, xmax = lngFloat + 0.01, ymax = latFloat + 0.01)
-  arc2(date, box = box)
+   if(nchar(latMax) > 0) { latMaxFloat <- round(as.numeric(latMax), digits = 1) }
+   else                  { latMaxFloat <- latMinFloat }
+   
+   if(nchar(lngMax) > 0) { lngMaxFloat <- round(as.numeric(lngMax), digits = 1) }
+   else                  { lngMaxFloat <- lngMinFloat }
+   
+   box <- c(xmin = lngMinFloat, ymin = latMinFloat, xmax = lngMaxFloat + 0.01, ymax = latMaxFloat + 0.01)
+
+   arc2(date, box = box)
 }
