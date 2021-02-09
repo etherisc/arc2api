881b89e52e33852dc81bec36ef035ec0076dc3df
Matthias Zimmermann <zimmermann@xbrain.ch>
2021-02-09 21:38:59 +0100

    Update arc2api.R
    extend documentation

--- app/arc2api.R
+++ app/arc2api.R
@@ -1,11 +1,16 @@
-# Title     : Arc2 API
-# Objective : Return precipitation for Africa for given date, latitude and longitude
-# Created by: Christoph Mussenbrock
+# @apiTitle       Arc2 REST API
+# @apiDescription Return precipitation for Africa for given date, latitude and longitude
+# @apiContact     list(name = "Contact", url = "https://github.com/etherisc/arc2api", email = "christoph.mussenbrock@etherisc")
 # Created on: 06/02/2021
 
 library(rnoaa)
 
-#* Return the precipitation at a date for lat, lng bounding box location. When maximum parameters are skipped only the value for the mininum latitude/longitude is returned.
+#* Return the precipitation at a date for lat, lng bounding box location. 
+#* When maximum parameters are skipped only the value for the mininum latitude/longitude is returned.
+#* Example data for some precipitation in Nairobi in April 2020
+#* - date: 2020-04-18
+#* - lat/long bounding box min values: 36.6, -1.5
+#* - lat/long bounding box max values: 37.1, -1.1 
 #* @param date date
 #* @param latMin minimum latitude of bounding box
 #* @param lngMin minimum longitude of bounding box
